d07f46bd8ee9ed81e20f9297c0574056f300825a
Grégoire Biendiné <gbiendine@adfinitas.fr>
2023-06-26 16:08:01 +0200

    push base svelte project with TailwindCSS

---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<div class="w-full h-screen flex flex-col justify-center items-center">
+    <h1 class="font-sans text-xl font-bold">Web Devtools</h1>
+    <p class="font-sans text-base">Set of tools for web developers</p>
+</div>
