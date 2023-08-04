3f33d5e28e11a23c983eef8a9ff47434d819daf7
Grégoire Biendiné <gbiendine@adfinitas.fr>
2023-08-04 13:44:53 +0200

    feat: add opacity slider in rgbatorgb page

--- src/lib/types/Color.ts
+++ src/lib/types/Color.ts
@@ -5,12 +5,25 @@ export type Color = RGB | RGBA | HEX;
 export type ColorObject = {r: number, g: number, b: number, a: number};
 
 export function ColorToOject(color: Color): ColorObject {
-    const cut = color.replace('#', '');
+    if (color.includes('#')) {
+        return {
+            r: parseInt(color.slice(1, 3), 16),
+            g: parseInt(color.slice(3, 5), 16),
+            b: parseInt(color.slice(5, 7), 16),
+            a: 1
+        };
+    }
+    const splitted = color.split('(')[1].split(')')[0].split(', ');
+    const nbs = splitted.map(el => {return parseFloat(el)});
+    if (nbs.length == 3)
+        return {r: nbs[0], g: nbs[1], b: nbs[2], a: 1};
+    return {r: nbs[0], g: nbs[1], b: nbs[2], a: nbs[3]};
+}
 
-    return {
-        r: parseInt(cut.slice(0, 2), 16),
-        g: parseInt(cut.slice(2, 4), 16),
-        b: parseInt(cut.slice(4, 6), 16),
-        a: 1
-    };
+export function NumberToHex(nb: number): string {
+    return Math.floor(nb).toString(16);
+}
+
+export function isValidHexColor(color: string): boolean {
+    return color.match(/^#([0-9a-f]{6})$/i) !== null;
 }
--- src/routes/rgbatorgb/+page.svelte
+++ src/routes/rgbatorgb/+page.svelte
@@ -1,21 +1,39 @@
 <script lang="ts">
-    import { ColorToOject, type Color, type ColorObject } from "$lib/types/Color";
+    import ColorPickerBox from "$lib/components/ColorPickerBox.svelte";
+    import { ColorToOject, NumberToHex, isValidHexColor, type Color, type ColorObject, type HEX } from "$lib/types/Color";
 
-    console.log(ColorToOject('#FF7D00'))
+    let opacity: number = 1;
 
-    function RGBAtoRGB(backgroundColor: Color, RGBAColor: Color) {
+    function RGBAtoRGB(backgroundColor: Color, RGBAColor: Color): HEX {
         const bgColor: ColorObject = ColorToOject(backgroundColor);
         const rgbaColor: ColorObject = ColorToOject(RGBAColor);
         const a = rgbaColor.a;
     
-        return `#
-            ${(1 - a) * bgColor.r + a * rgbaColor.r}
-            ${(1 - a) * bgColor.g + a * rgbaColor.g}
-            ${(1 - a) * bgColor.b + a * rgbaColor.b}
-        `;
+        return `#${NumberToHex((1 - a) * bgColor.r + a * rgbaColor.r)}${NumberToHex((1 - a) * bgColor.g + a * rgbaColor.g)}${NumberToHex((1 - a) * bgColor.b + a * rgbaColor.b)}`;
     }
 </script>
 
-<section class="">
-
+<section class="w-full h-screen flex justify-center items-center">
+    <div class="flex flex-col justify-center items-center pb-12">
+        <div class="flex flex-row justify-center items-center gap-16 mb-12">
+            <ColorPickerBox title="RGB Background"/>
+            <ColorPickerBox title="RGB Color"/>
+        </div>
+        <p class="font-sans text-base font-bold">Opacity : {opacity}</p>
+        <input class="sliderOpacity w-full h-3 rounded-full mt-4 appearance-none outline-none opacity-70 transition-opacity duration-300 hover:opacity-100" type="range" min="0" max="1" step="0.01" bind:value={opacity}>
+    </div>
 </section>
+
+<style lang="postcss">
+    .sliderOpacity {
+        background: #d3d3d3;
+    }
+
+    .sliderOpacity::-webkit-slider-thumb {
+        @apply appearance-none w-5 h-5 rounded-full cursor-pointer bg-soft-purple-2;
+    }
+
+    .sliderOpacity::-moz-range-thumb {
+        @apply appearance-none w-5 h-5 rounded-full cursor-pointer bg-gray-500;
+    }
+</style>
