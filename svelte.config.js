02dd7daa76d91a2aa4e712156fe31995961b2c7f
Grégoire Biendiné <60387414+gregoirebiendine@users.noreply.github.com>
2024-04-26 02:31:15 +0200

    Update packages and deploy.yml

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +8,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		paths: {
-            'base': process.argv.includes('dev') ? '' : '/webdevtools',
+            base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
         }
 	}
 };
